717c530721cfe13a48ad0bd6e311c37fd81563c6
Sagar <shindesagar575@gmail.com>
2024-08-15 16:37:50 +0530

    Add printFarewell method to Greeting.java

--- Greeting.java
+++ Greeting.java
@@ -1,5 +1,10 @@
 public class Greeting {
     public static void main(String[] args) {
         System.out.println("Hello! This is a new feature in the add-greeting-feature branch.");
+        printFarewell();
+    }
+
+    public static void printFarewell() {
+        System.out.println("Goodbye! See you next time.");
     }
 }
