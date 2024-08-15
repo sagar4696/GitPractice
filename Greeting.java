48789fc7a8614a086dea0f8ed05b671f8c17302f
Sagar <shindesagar575@gmail.com>
2024-08-15 20:27:08 +0530

    Added new line to understand Git diff command

--- Greeting.java
+++ Greeting.java
@@ -2,6 +2,7 @@ public class Greeting {
     public static void main(String[] args) {
         System.out.println("Hello! This is a new feature in the add-greeting-feature branch.");
         printFarewell();
+        System.out.println("Hello! This is an updated feature message.");
     }
 
     public static void printFarewell() {
